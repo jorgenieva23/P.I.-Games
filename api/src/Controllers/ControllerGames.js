6d0b40296ee5e41957d05e19cc324796d3c7c2a7
jorgenieva23 <jorge_4755@hotmail.com>
2023-02-13 14:24:28 -0300

    1er projecto

--- api/src/Controllers/ControllerGames.js
+++ api/src/Controllers/ControllerGames.js
@@ -1,10 +1,23 @@
 const  axios = require('axios');
 const {videogame, Genres} = require('../db.js');
-const {API_KEY} = process.env;
+require("dotenv").config();
+const { API_KEY } = process.env;
 const {Router} = require ('express');
-const router = Router();
+// const router = Router();
 
-const cleanArray = (arr) =>{
+const cleanArray = (arr) => arr.map((e)=>{
+    return{
+        id:e.id,
+            // name:e.name,
+            // image:e.background_image,
+            // // description:e.description,
+            // release_date:e.release_date,
+            // genres: e.genres.map((el) => el.name),
+            // create:false
+        }
+    })
+
+const cleanArray2 = (arr) =>{
     arr.map((e)=>{
         return{
             id:e.id,
@@ -12,43 +25,35 @@ const cleanArray = (arr) =>{
             image:e.background_image,
             // description:e.description,
             release_date:e.release_date,
-            genre:e.genre,
+            genres: el.dataValues.genres.map((e) => ( e.dataValues.name )),
             create:false
         }
     })
 }
 
-const cleanArray2 = (arr) =>{
-    arr.map((e)=>{
-        return{
-            id:e.id,
-            name:e.name,
-            image:e.image,
-            // description:e.description,
-            release_date:e.release_date,
-            genre:e.genre,
-            create:false
-        }
-    })
-}
-
-const getAllGames = async () => {
-    const databaseGames = await videogame.findAll({
-        include: {
-            model: Genres,
-            attributes: ['name'],
-            through: {
-                attributes: []
-            }
-        }
-    });
-    const apiGamesRaw = (await axios.get(`https://api.rawg.io/api/games?key=2a9362cde1e9425f9100cb457aad7d42&page=${i}`)).data;
-
-    const apiGames = cleanArray(apiGamesRaw)
-
-    const bddGames = cleanArray2(databaseGames)
     
-    return [...apiGames, ...bddGames]
+const getAllGames = async () => {
+    
+    const apiGamesRaw = (await axios.get(`https://api.rawg.io/api/games?key=2a9362cde1e9425f9100cb457aad7d42`));
+    
+    const apiGames = apiGamesRaw.data.results
+    // console.log("hola", apiGames);
+    
+    const apiGamesClean = cleanArray(apiGames)
+    console.log(apiGamesClean);
+    
+    // const bddGames = cleanArray2(databaseGames)
+    // const databaseGames = await videogame.findAll({
+    //     include: {
+    //         model: Genres,
+    //         attributes: ['name'],
+    //         through: {
+    //             attributes: []
+    //         }
+    //     }
+    // });
+    
+    return [...apiGamesClean]
 }
 
 const searchGameByName = async (name) => {
@@ -60,22 +65,38 @@ const searchGameByName = async (name) => {
         through: {
             attributes: []
             }
-        }] 
-    });
+        }],
+    attributes: ["id", "name",],
+        where: {
+          name: {
+            [Op.iLike]: `%${name}%`,
+          },
+        },
+      });
 
     const bddGames = cleanArray2(databaseGames)
+    
     const apiGamesRaw = (await axios.get("https://api.thedogapi.com/v1/breeds")).data;
 
-    const apiGames = cleanArray(apiGamesRaw);
-
     const filteredApi = apiGames.filter((e) => e.name.toLowerCase().includes(name.toLowerCase()));
+    
+    const apiGames = cleanArray(apiGamesRaw);
 
     return [...filteredApi, ...bddGames];
 };
 
+// const b = undefined
+// const v = ["a"]
+// console.log([...b, ...v]);
 
+let a = (a) =>{
+    console.log("hola");
+};
 
-module.exports={searchGameByName, getAllGames}
+const b = () => a 
+b()
+
+module.exports = {searchGameByName, getAllGames}
 
 
 
